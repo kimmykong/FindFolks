8bee5c29ffa3d7a52fde4a99a2d0cd72dc4d3460
Kim <kimmykong123@gmail.com>
2016-12-10 01:34:59 -0500

    interest pages

--- init.py
+++ init.py
@@ -8,27 +8,21 @@ app = Flask(__name__)
 # Configure MySQL
 conn = pymysql.connect(host='localhost',
                        user='root',
-                       password='root',
+                       # password='root',
                        db='findfolks',
                        charset='utf8mb4',
                        cursorclass=pymysql.cursors.DictCursor)
 
-
 # Define a route to hello function
 @app.route('/')
 def hello():
-    cursor = conn.cursor()
-    query = 'SELECT title FROM an_event'
-    cursor.execute(query)
-    data = cursor.fetchall()
-    cursor.close()
-    return render_template('index.html', event = data)
+    return renderIndexPage()
+
 
 # Define route for login
 @app.route('/login')
 def login():
     return render_template('login.html')
-
 
 # Define route for register
 @app.route('/register')
@@ -62,7 +56,6 @@ def loginAuth():
         error = 'Invalid login or username'
         return render_template('login.html', error=error)
 
-
 # Authenticates the register
 @app.route('/registerAuth', methods=['GET', 'POST'])
 def registerAuth():
@@ -94,7 +87,21 @@ def registerAuth():
         cursor.execute(ins, (username, password, firstname,lastname,email,zipcode))
         conn.commit()
         cursor.close()
-        return render_template('index.html')
+        return renderIndexPage()
+
+def renderIndexPage():
+    cursor = conn.cursor()
+
+    query = 'SELECT * FROM interest'
+    cursor.execute(query)
+    interests = cursor.fetchall()
+
+    query = 'SELECT title FROM an_event'
+    cursor.execute(query)
+    events = cursor.fetchall()
+
+    cursor.close()
+    return render_template('index.html', interests=interests, event=events)
 
 
 @app.route('/home')
@@ -102,7 +109,7 @@ def home():
     username = session['username']
     cursor = conn.cursor();
     # displays events in the next three days by default
-    query = 'SELECT * FROM sign_up JOIN an_event ON sign_up.event_id = an_event.event_id WHERE sign_up.username = %s AND NOW() < an_event.start_time < DATE_ADD(NOW(),INTERVAL 3 DAY)' 
+    query = 'SELECT * FROM sign_up JOIN an_event ON sign_up.event_id = an_event.event_id WHERE sign_up.username = %s AND NOW() < an_event.start_time < DATE_ADD(NOW(),INTERVAL 3 DAY)'
     cursor.execute(query, (username))
     data = cursor.fetchall()
     cursor.close()
@@ -112,10 +119,24 @@ def home():
     # data = cursor.fetchall()
     # cursor.close()
 
-    return render_template('home.html', username=username, event = data) 
+    return render_template('home.html', username=username, event=data)
 
-    
 
+@app.route('/sandbox')
+def sandbox():
+    cursor = conn.cursor()
+
+    query = 'SELECT * FROM interest'
+    cursor.execute(query)
+    data = cursor.fetchall()
+
+    cursor.close()
+    return render_template('sandbox.html', data=data)
+
+@app.route('/interest/<categoryKeyword>', methods=['GET','POST'])
+def interest(categoryKeyword):
+    catKey = str(categoryKeyword).split("+")
+    return render_template('interest.html', category=catKey[0], keyword=catKey[1] )
 
 
 # @app.route('/post', methods=['GET', 'POST'])
